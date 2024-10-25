7491625757f3016d1310871bc1f405d2ab6c962e
Jean Gravier <jean.gravier@ubisoft.com>
2024-10-25 20:52:42 +0200

    chore: unnecessary absolute path

--- compile-shaders.sh
+++ compile-shaders.sh
@@ -3,8 +3,8 @@
 set -e
 
 cd shaders
-/c/VulkanSDK/1.3.290.0/Bin/glslc tri.vert -o vert.spv
-/c/VulkanSDK/1.3.290.0/Bin/glslc tri.frag -o frag.spv
+glslc tri.vert -o vert.spv
+glslc tri.frag -o frag.spv
 cd ..
 
 echo "OK"
