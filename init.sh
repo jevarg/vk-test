609c25f0bc113cbdf6069e2d057785ce1f23ce5d
Jean Gravier <jevarg@pm.me>
2025-01-26 23:03:30 +0100

    fix: multi platform build

--- init.sh
+++ init.sh
@@ -2,9 +2,20 @@
 
 set -e
 
+OS_NAME=$(uname -s)
+
+case "$OS_NAME" in
+  Darwin*)
+    export PROFILE="apple-silicon"
+    ;;
+  *)
+    echo "Unsupported platform! $OS_NAME"
+    exit 1
+esac
+
 PROJECT_DIR=$(dirname "$(readlink -f "$0")")
 export CONAN_HOME="$PROJECT_DIR/.conan"
 
 conan profile detect --exist-ok
-conan install --build=missing --profile msvc --profile debug conanfile.txt
-conan install --build=missing --profile msvc --profile release conanfile.txt
+conan install --build=missing --profile "$PROFILE" --profile debug conanfile.txt
+conan install --build=missing --profile "$PROFILE" --profile release conanfile.txt
